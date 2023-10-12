6ded25eca0b859c015de53e1cc5909e5632491a4
Amadeus Kiener <a.kiener@unibrand.de>
2023-10-12 11:37:51 +0200

    add FontAwesomeIcons plugin

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -7,5 +7,6 @@ return [
     ],
     'imports' => [
         '@ubos/ckeditor-icons' => 'EXT:ckeditor_icons/Resources/Public/JavaScript/CkEditor5/Icons/Icons.js',
+        '@ubos/ckeditor-fontawesome-icons' => 'EXT:ckeditor_icons/Resources/Public/JavaScript/CkEditor5/FontAwesomeIcons/FontAwesomeIcons.js',
     ],
 ];
