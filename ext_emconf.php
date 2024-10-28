d0d6f3263e8346a24841a0e3d91192d448fc9a85
Amadeus Kiener <a.kiener@unibrand.de>
2024-10-28 12:33:01 +0100

    update version

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,15 +3,15 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'CKEditor 5: Icons Plugin',
     'description' => 'Adds an icon dropdown to CKEditor in TYPO3.',
-    'version' => '0.1.0',
+    'version' => '0.2.5',
     'state' => 'stable',
     'category' => 'be',
     'author' => 'Amadeus Kiener',
     'author_email' => 'amd.kiener@gmail.com',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
-            'rte_ckeditor' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.9.99',
+            'rte_ckeditor' => '12.4.0-13.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
